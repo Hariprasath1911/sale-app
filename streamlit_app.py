05cf5fcf289b4bfe52941494fbcdc18f1e6745e1
Hariprasath1911 <hari07011996@gmail.com>
2025-01-05 13:37:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,27 @@ def load_model():
 
 model = load_model()
 
+def set_background_image_local(image_path):
+    with open(image_path, "rb") as file:
+        data = file.read()
+    base64_image = base64.b64encode(data).decode("utf-8")
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/png;base64,{base64_image}");
+            background-size: cover;
+            background-position: fit;
+            background-repeat: repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+set_background_image_local(r"s1.png")
+
 # Streamlit App Title
 st.title("Predicting House Prices Based on Past Sales Data")
 st.image("https://raw.githubusercontent.com/Hariprasath1911/sale-app/main/1.png")
